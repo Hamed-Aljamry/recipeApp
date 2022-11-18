ad4fe1f547deb16c51a8ee65b595bc0de70de526
hamed aljamry <hamed970@hotmail.com>
2022-11-18 12:44:01 +0000

    three main fucntion for app

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("hi")
+getRandomMeal();
+
+async function getRandomMeal(){
+  const resp = await fetch(
+    'www.themealdb.com/api/json/v1/1/random.php'
+    );
+    const randomMeal = await resp.json();
+console.log(randomMeal);
+
+}
+
+async function getMealById(id){
+  const meal = await fetch(
+    'www.themealdb.com/api/json/v1/1/lookup.php?i=52772'+id
+    );
+
+}
+
+async function getMealsBySearch(term){
+  const meals = await fetch(
+    'www.themealdb.com/api/json/v1/1/search.php?s='+term
+  );
+
+}
