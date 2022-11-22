8baa0b92cf97df11cc9427388c55fe812c4e13f6
hamed aljamry <hamed970@hotmail.com>
2022-11-22 22:33:09 +0000

    addMeal function, to get a random meal

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const meals = document.getElementById("meals");
+
 getRandomMeal();
 
 async function getRandomMeal(){
@@ -5,10 +7,12 @@ async function getRandomMeal(){
     'https://www.themealdb.com/api/json/v1/1/random.php'
   );
 
-    const randomMeal = await resp.json();
+    const respData = await resp.json();
+    const randomMeal = respData.meals[0]
 
     console.log(randomMeal);
 
+    addMeal(randomMeal, true);
 }
 
 async function getMealById(id){
@@ -21,4 +25,22 @@ async function getMealById(id){
 async function getMealsBySearch(term){
   const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+term);
 
+}
+
+
+addMeal(mealData, random = false){
+  const meal = document.createElement('div');
+  meal.classList.add('meal')
+
+  meal.innerHTML = `
+  <div class="meal-header">
+   ${random ? ` <span class="random">
+   Random Recipe
+ </span>` : ''}
+    <img src="${mealData.strMealThumb}" alt="${mealData.Meal}">
+  </div>
+  <div class="meal-body">
+    <h4>${mealData.Meal}</h4>
+    <button class="fav-btn"> <i class="fas fa-heart"></i></button>
+  </div>`
 }
